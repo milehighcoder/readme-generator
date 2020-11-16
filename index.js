60558fae873736c4d4161383bdfe067062c698c7
mykereel <mgmartnz@icloud.com>
2020-11-16 16:09:32 -0700

    readme generating

--- index.js
+++ index.js
@@ -1,54 +1,55 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type: 'input',
-        message: 'What is the project title?',
-        name: 'name',
+      type: "input",
+      message: "What is the project title?",
+      name: "title",
     },
     {
-        type: 'input',
-        message: 'Enter a general description of the project.',
-        name: 'description'
+      type: "input",
+      message: "Enter a general description of the project.",
+      name: "description",
     },
     {
-        type: 'input',
-        message: 'Enter installation instructions.',
-        name: 'installation instructions'
+      type: "input",
+      message: "Enter installation instructions.",
+      name: "installation instructions",
     },
     {
-        type: 'input',
-        message: 'Enter usage information.',
-        name: 'usage information'
+      type: "input",
+      message: "Enter usage information.",
+      name: "usage information",
     },
     {
-        type: 'input',
-        message: 'Enter contribution guidelines.',
-        name: 'contribution guidelines'
+      type: "input",
+      message: "Enter contribution guidelines.",
+      name: "contribution guidelines",
     },
     {
-        type: 'input',
-        message: 'Enter test instructions.',
-        name: 'test instructions'
+      type: "input",
+      message: "Enter test instructions.",
+      name: "test instructions",
     },
     {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'githuh username'
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "githuh username",
     },
     {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email address'
+      type: "input",
+      message: "What is your email address?",
+      name: "email address",
     },
+  ])
 
-])
-
-// array of questions for user
-// const questions = [
-
-// ];
+  .then((data) => {
+    fs.writeFile("README.md", `# ${data.title}`, (err) =>
+      err ? console.log(err) : console.log("README successfully created!")
+    );
+  });
 
 // function to write README file
 // function writeToFile(fileName, data) {
