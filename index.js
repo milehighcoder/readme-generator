f630e2e43af562cbabb9da4fcca12879d905e6d0
mykereel <mgmartnz@icloud.com>
2020-11-16 15:53:53 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the project title?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Enter a general description of the project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions.',
+        name: 'installation instructions'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information.',
+        name: 'usage information'
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines.',
+        name: 'contribution guidelines'
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions.',
+        name: 'test instructions'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'githuh username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email address'
+    },
+
+])
+
+// array of questions for user
+// const questions = [
+
+// ];
+
+// function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// function to initialize program
+// function init() {
+
+// }
+
+// function call to initialize program
+// init();
